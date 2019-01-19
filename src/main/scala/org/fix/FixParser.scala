5e44bd31122fa45480855d22459678704f6b8160
Constantin Mitran <mitranc@gmail.com>
2019-01-19 08:42:43 +0000

    fix parser - refactored field extraction

--- src/main/scala/org/fix/FixParser.scala
+++ src/main/scala/org/fix/FixParser.scala
@@ -5,19 +5,15 @@ trait FixParser {
 }
 
 case class SimpleFixParser() extends FixParser {
-  override def parse(schema: FixSchema, fix: String): FixMessage = {
-    val emptyMessage = FixMessage()
-    Option(fix)
-      .fold(
-        emptyMessage
-      ) {
-        case s if s.isEmpty => emptyMessage
-        case m => FixMessage(schema, extractFields(m))
-      }
-  }
+  override def parse(schema: FixSchema, fix: String): FixMessage = FixMessage(schema, extractFields(fix))
 
-  private def extractFields(fix: String): Seq[FixField] = fix
-    .split(FixMessage.fieldDelimiter)
-    .map(_.split("="))
-    .map((t: Array[String]) => Field(t(0).toInt, t(1))).toList
+  private def extractFields(fix: String): Seq[FixField] = Option(fix)
+    .fold(
+      List[FixField]()
+    )(m => if (m.isEmpty)
+      List[FixField]()
+    else
+      m.split(FixMessage.fieldDelimiter)
+        .map(_.split("="))
+        .map((t: Array[String]) => Field(t(0).toInt, t(1))).toList)
 }
