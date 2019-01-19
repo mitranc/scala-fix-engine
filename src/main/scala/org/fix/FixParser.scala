99ac071b7e5f3b866e9682dc238b6419c895a23b
Constantin Mitran <mitranc@gmail.com>
2019-01-19 09:12:54 +0000

    fix parser - parsing using message def

--- src/main/scala/org/fix/FixParser.scala
+++ src/main/scala/org/fix/FixParser.scala
@@ -6,10 +6,22 @@ trait FixParser {
 
 case class SimpleFixParser() extends FixParser {
   override def parse(schema: FixSchema, fix: String): FixMessage = {
-    FixMessage(schema, parse(schema, extractFields(fix)))
+    FixMessage(schema, parseFields(schema, extractFields(fix)))
   }
 
-  private def parse(schema: FixSchema, fields: Seq[FixField]): Seq[FixField] = fields
+  private def parseFields(schema: FixSchema, fields: Seq[FixField]): Seq[FixField] = fields
+    .find(f => f.tag == 35)
+    .map(_.value.toString)
+    .fold(
+      fields
+    ) { msgType =>
+      schema.messageDefs
+        .find(md => md.msgType == msgType)
+        .fold(fields)(md => parseMessage(md, fields))
+    }
+
+  def parseMessage(messageDef: MessageDef, fields: Seq[FixField]): Seq[FixField] = fields
+
   private def extractFields(fix: String): Seq[FixField] = Option(fix)
     .fold(
       List[FixField]()
